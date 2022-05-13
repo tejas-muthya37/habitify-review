86a5b78489ca2c3e979f3cb880a32eeeeb00ec01
tejas-muthya37 <55662912+tejas-muthya37@users.noreply.github.com>
2022-05-13 13:03:37 +0530

    Feat: Configuring App.js, index.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,60 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./Screens/Homepage/Homepage";
+import Mockman from "mockman-js";
+import Authenticate from "./Screens/Authenticate/Authenticate";
+import RequiresAuth from "./RequiresAuth";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/mock" element={<Mockman />} />
+        <Route
+          path="/"
+          element={
+            <RequiresAuth>
+              <Homepage morningHabits={false} archivedHabits={false} />
+            </RequiresAuth>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Authenticate
+              cardTitle="SIGN IN"
+              alternate="Create A New Account"
+            />
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <Authenticate
+              cardTitle="SIGN UP"
+              alternate="Login With Existing Account"
+            />
+          }
+        />
+        <Route
+          path="/habits/morning"
+          element={
+            <RequiresAuth>
+              <Homepage morningHabits={true} />
+            </RequiresAuth>
+          }
+        />
+        <Route
+          path="/habits/archived"
+          element={
+            <RequiresAuth>
+              <Homepage archivedHabits={true} />
+            </RequiresAuth>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HabitProvider } from "./Context/habit-context";
+import { PomodoroProvider } from "./Context/pomodoro-context";
+import { TokenProvider } from "./Context/token-context";
+import { ToastProvider } from "./Context/toast-context";
 import { makeServer } from "./server";
 import App from "./App";
 
@@ -7,7 +11,15 @@ makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HabitProvider>
+      <PomodoroProvider>
+        <TokenProvider>
+          <ToastProvider>
+            <App />
+          </ToastProvider>
+        </TokenProvider>
+      </PomodoroProvider>
+    </HabitProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
